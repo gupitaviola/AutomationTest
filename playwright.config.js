05faf1ced503609e044c18b85f051a296cddc5a3
Gupita Viola Adeline <gupitav@Gupitas-MacBook-Air.local>
2025-05-28 12:32:31 +0700

    implement allure reporter

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['allure-playwright'],
+    ['html', { open: 'never' }] // optional, for HTML report
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
